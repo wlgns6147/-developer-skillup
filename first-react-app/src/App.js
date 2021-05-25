4a73531431bd9549c77b5600b1698df8486630e2
황지훈 <wlgns6147@douzone.com>
2021-05-25 10:40:05 +0900

    react파일 수정

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 
 function App() {
   // valuebox = 계산 값
-  const [valuebox, setValuebox] = useState([0]);
+  const [valuebox, setValuebox] = useState(0);
   // inputbox = 입력 값
   const [inputbox, setInputbox] = useState(0);
   // 현재 위치 표시 index
   const [index, setIndex] = useState(0);
+  // undo, redo를 위한 배열
+  const [arr, setArr] = useState([0]);
   // inputbox 값 변경 이벤트
   const onChange = (e) => {
     setInputbox(e.target.value);
@@ -15,37 +17,52 @@ function App() {
   // 버튼 클릭 이벤트
   const onClick = (type) => {
     if (type === "add" || type === "sub") {
-
-      console.log("valuebox=" + valuebox);
-      console.log("inputbox=" + inputbox);
-      console.log("index=" + index);
-
-
-      const indexValue = valuebox[index];
-      const tempArr = valuebox.splice(0, index + 1);
-
-      if (type === "add") {
-        tempArr.push(indexValue + Number(inputbox));
+      if (inputbox == '' || inputbox == null) {
+        return;
       }
       else {
-        tempArr.push(indexValue - Number(inputbox));
+        if (type === "add") {
+          setValuebox(Number(valuebox) + Number(inputbox));
+        }
+        else {
+          setValuebox(Number(valuebox) - Number(inputbox));
+        }
+
+        if (arr.length > 1 && arr.length > index) {
+          for (var i = arr.length; i > index + 1; i--) {
+            arr.pop();
+          }
+        }
+
+        // arr 배열 값 추가
+        arr.push(Number(valuebox) + Number(inputbox));
+        setIndex(index + 1);
       }
-      setValuebox(tempArr);
-      setIndex(index + 1);
-      setInputbox(0);
+      setInputbox('');
     }
     else if (type === "undo") {
-      setIndex(index - 1);
+      if (index >= 1) {
+        setValuebox(arr[index - 1]);
+        setIndex(index - 1);
+      }
     }
     else if (type === "redo") {
-      setIndex(index + 1);
+      if (index < arr.length - 1) {
+        setValuebox(arr[index + 1]);
+        setIndex(index + 1);
+      }
     }
+    console.log("valuebox = " + valuebox);
+    console.log("inputbox = " + inputbox);
+    console.log("index = " + index);
+    console.log("arr = " + arr);
+    console.log("==================");
   };
 
   return (
     <div className="container">
       <div id="valuebox" className="counter">
-        {valuebox[index]}
+        {valuebox}
       </div>
       <input id="inputbox" type="number" className="input" value={inputbox} onChange={onChange} />
       <div className="btnGroup">
@@ -58,7 +75,7 @@ function App() {
         <button id="subButton" className="btn" onClick={() => onClick("sub")}>
           -
         </button>
-        <button id="redoButton" className="btn" disabled={index >= valuebox.length - 1} onClick={() => onClick("redo")}>
+        <button id="redoButton" className="btn" disabled={index >= arr.length - 1} onClick={() => onClick("redo")}>
           Redo
         </button>
       </div>
