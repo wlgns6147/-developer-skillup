c5c3c1a79b2c4d2029dfbdbb48bbd201269e09dc
황지훈 <wlgns6147@douzone.com>
2021-05-28 15:54:58 +0900

    <div class="col right"> 누락

--- address-book/src/component/memberDetail.js
+++ address-book/src/component/memberDetail.js
@@ -1,16 +1,18 @@
 const MemberInfo = ({ selectedList }) => {
     return (
-        <div class="details">
-            {selectedList ? (
-                <ul class="info">
-                    <li>이름: {selectedList.name}</li>
-                    <li>부서: {selectedList.dept}</li>
-                    <li>휴대폰: {selectedList.phone}</li>
-                    <li>메일: {selectedList.mail}</li>
-                </ul>
-            ) : (
-                <p class="emptyset">정보가 없습니다.</p>
-            )}
+        <div class="col right">
+            <div class="details">
+                {selectedList ? (
+                    <ul class="info">
+                        <li>이름: {selectedList.name}</li>
+                        <li>부서: {selectedList.dept}</li>
+                        <li>휴대폰: {selectedList.phone}</li>
+                        <li>메일: {selectedList.mail}</li>
+                    </ul>
+                ) : (
+                    <p class="emptyset">정보가 없습니다.</p>
+                )}
+            </div>
         </div>
     );
 };
