28559235a12d178420fc69f47c460c9c1ced2c8d
황지훈 <wlgns6147@douzone.com>
2021-08-18 15:54:58 +0900

    멤버 변경

--- address-book/src/members.js
+++ address-book/src/members.js
@@ -1,39 +1,39 @@
 const members = [
-    {
-        id: 1,
-        name: "황지훈",
-        dept: "헬스케어솔루션사업본부",
-        phone: "010-1111-1111",
-        mail: "황지훈@wehago.com",
-    },
-    {
-        id: 2,
-        name: "임성후",
-        dept: "헬스케어솔루션사업본부",
-        phone: "010-2222-2222",
-        mail: "임성후@wehago.com",
-    },
-    {
-        id: 3,
-        name: "진보라",
-        dept: "헬스케어솔루션사업본부",
-        phone: "010-3333-3333",
-        mail: "진보라@wehago.com",
-    },
-    {
-        id: 4,
-        name: "김회준",
-        dept: "플랫폼사업부문",
-        phone: "010-4444-4444",
-        mail: "김회준@wehago.com",
-    },
-    {
-        id: 5,
-        name: "김동혁",
-        dept: "플랫폼사업부문",
-        phone: "010-5555-5555",
-        mail: "김동혁@wehago.com",
-    },
+  {
+    id: 1,
+    name: '박시현',
+    dept: '헬스케어솔루션사업본부',
+    phone: '010-1111-1111',
+    mail: '박시현@wehago.com',
+  },
+  {
+    id: 2,
+    name: '윤서영',
+    dept: '헬스케어솔루션사업본부',
+    phone: '010-2222-2222',
+    mail: '윤서영@wehago.com',
+  },
+  {
+    id: 3,
+    name: '이종현',
+    dept: '헬스케어솔루션사업본부',
+    phone: '010-3333-3333',
+    mail: '이종현@wehago.com',
+  },
+  {
+    id: 4,
+    name: '임도희',
+    dept: '플랫폼사업부문',
+    phone: '010-4444-4444',
+    mail: '임도희@wehago.com',
+  },
+  {
+    id: 5,
+    name: '조민상',
+    dept: '플랫폼사업부문',
+    phone: '010-5555-5555',
+    mail: '조민상@wehago.com',
+  },
 ];
 
 export default members;
