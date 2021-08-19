454c15f8b213adbbdc2064c4dd72e3579f9fedfa
황지훈 <wlgns6147@douzone.com>
2021-08-19 13:47:07 +0900

    # 교육자료 정리

--- 백업/address-book-next/data/members.ts
+++ 백업/address-book-next/data/members.ts
@@ -4,38 +4,38 @@
 let members = [
   {
     id: 1,
-    name: '황지훈',
+    name: '박시현',
     dept: '헬스케어솔루션사업본부',
     phone: '010-1111-1111',
-    mail: '황지훈@wehago.com',
+    mail: '박시현@wehago.com',
   },
   {
     id: 2,
-    name: '임성후',
+    name: '윤서영',
     dept: '헬스케어솔루션사업본부',
     phone: '010-2222-2222',
-    mail: '임성후@wehago.com',
+    mail: '윤서영@wehago.com',
   },
   {
     id: 3,
-    name: '진보라',
+    name: '이종현',
     dept: '헬스케어솔루션사업본부',
     phone: '010-3333-3333',
-    mail: '진보라@wehago.com',
+    mail: '이종현@wehago.com',
   },
   {
     id: 4,
-    name: '김회준',
+    name: '임도희',
     dept: '플랫폼사업부문',
     phone: '010-4444-4444',
-    mail: '김회준@wehago.com',
+    mail: '임도희@wehago.com',
   },
   {
     id: 5,
-    name: '김동혁',
+    name: '조민상',
     dept: '플랫폼사업부문',
     phone: '010-5555-5555',
-    mail: '김동혁@wehago.com',
+    mail: '조민상@wehago.com',
   },
 ];
 
